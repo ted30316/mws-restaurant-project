1654674ece59cb7066a284778c3fd676fc089bb3
ted30316 <tedwilliams@Teds-MacBook-Pro.local>
2019-02-12 07:31:07 -0500

    Enable map and api key secret

---
+++ js/secret.js
@@ -0,0 +1,6 @@
+class SECRET {
+  static get mapbox_key() {
+    //return your mapbox api key
+    return "pk.eyJ1IjoidGFlODI3IiwiYSI6ImNqczFvZDBjbjF3b2gzeW8zeXZsbzU1ZmUifQ.qLHFj9A5uuKEQE8OQekdsQ";
+  }
+}
